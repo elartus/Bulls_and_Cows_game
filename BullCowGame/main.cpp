b5c5722e7dd42b150a6a6efb54108fc294f61f09
Arturo <art3d70@gmail.com>
2016-12-02 01:24:32 -0600

    C16 - Character Input introduction

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
+	//introduce the game
 	constexpr int WORLD_LENGHT = 5;
-	cout << "Welcome to Bulls and Cows, a fun word game" << endl;
+	cout << "Welcome to Bulls and Cows, a fun word game \n";
 	cout << "Can you guess the " << WORLD_LENGHT;
-	cout << " letter isogram I´m thinking of?" << endl;
+	cout << " letter isogram I´m thinking of? \n";
 
+	//get a guess from the player
+	string Guess = "";
+	cout << "Guess: ";	
+	cin >> Guess;
+	
+	//repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	//get a guess from the player
+	cout << "Guess: ";
+	cin >> Guess;
+
+	//repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
 	return 0;
 }
