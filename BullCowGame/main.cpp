89687130272f5ee7c8bb665bdd48f5b9d6ebb885
Arturo <art3d70@gmail.com>
2016-12-02 01:04:19 -0600

    Welcome screen

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 
 int main() {
+	constexpr int WORLD_LENGHT = 5;
+	cout << "Welcome to Bulls and Cows, a fun word game" << endl;
+	cout << "Can you guess the " << WORLD_LENGHT;
+	cout << " letter isogram I´m thinking of?" << endl;
 
-	cout << "Welcome to Bulls and Cows" << endl;
 	return 0;
 }
