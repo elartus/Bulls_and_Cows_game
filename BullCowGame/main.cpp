ed7b29dee7de9380f2166ea85110f82b51e5a1c3
Arturo <art3d70@gmail.com>
2016-12-03 01:12:19 -0600

    C18 Simplificando con funciones

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -3,29 +3,40 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
 int main() {
-	//introduce the game
+	
+	PrintIntro();
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+		
+	cout << endl;
+	return 0;
+
+
+}
+
+//introduce the game
+void PrintIntro() {
+
 	constexpr int WORLD_LENGHT = 5;
 	cout << "Welcome to Bulls and Cows, a fun word game \n";
 	cout << "Can you guess the " << WORLD_LENGHT;
 	cout << " letter isogram I´m thinking of? \n";
+	return;
+}
 
-	//get a guess from the player
+//get a guess from the player
+string GetGuessAndPrintBack() {
+	
 	cout << "Enter you Guess: ";
 	string Guess = "";
-	getline(cin, Guess);	
-	
-	//repeat the guess back to them
-	cout << "Your guess was: " << Guess << endl;
-
-	//get a guess from the player
-	cout << "Enter you Guess: ";
 	getline(cin, Guess);
 
-	//repeat the guess back to them
+	//print the guess back
 	cout << "Your guess was: " << Guess << endl;
-	
 
-	cout << endl;
-	return 0;
+	return Guess;
 }
